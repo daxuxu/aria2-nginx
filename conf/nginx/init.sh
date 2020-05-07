841cd84df96f9c77b526c9901838567c4d0cda1f
daxuxu <mrxurui@gmail.com>
2020-05-07 21:09:15 +0800

    美化文件列表输出

---
+++ conf/nginx/init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#mkdir -p /root/.aria2
+
+#cp /aria2/conf/* /root/.aria2/
+
+cp /aria2/conf/nginx/autoindex.html /aria2/downloads/
+
+if [ $SECRET ]; then
+    echo "rpc-secret=${SECRET}" >> /root/.aria2/aria2.conf
+fi
+echo "admin:$(openssl passwd -crypt admin)" > /.htpasswd
+if [ $HTPASSWD ]; then
+    echo "admin:$(openssl passwd -crypt $HTPASSWD)" > /.htpasswd
+fi
+
+touch /root/.aria2/aria2.session
+
+sh /root/.aria2/update-bt-tracker.sh
+
+nginx -c /aria2/conf/nginx/nginx.conf  
+aria2c --conf-path=/root/.aria2/aria2.conf
